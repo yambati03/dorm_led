4bd528c8384a3adbe9d6720f510da9955d923a18
Yashas Ambati <yashas.amb@gmail.com>
2020-07-10 10:12:52 -0400

    updated frontend, added more functionality

--- app.py
+++ app.py
@@ -14,19 +14,27 @@ def hex_to_rgb(hex):
     t_color = tuple(int(h[i:i+2], 16) for i in (0, 2, 4))
     return Color(t_color[0], t_color[1], t_color[2])
 
-@app.route('/controller', methods=['GET', 'POST'])
+@app.route('/controller', methods=["GET", "POST"])
 def mode_dropdown():
+    mode = None
     if request.method == "POST":
         mode = request.form["selector"]
         color = request.form["color"]
-        if mode == 'solid_color':
-            color = hex_to_rgb(color)
-            controller.change_mode(mode, set_color = color)
-        else:
-            controller.change_mode(mode)
+        brightness = request.form["brightness"]
+        switch = request.form.get("switch")
 
-    modes = ['rainbow_run', 'rainbow_fade', 'solid_color', 'off', 'on']
-    return render_template('app.html', modes=modes)
+        if switch == "on":
+            if mode == 'solid_color':
+                color = hex_to_rgb(color)
+                controller.change_mode(mode, set_color = color)
+            else:
+                controller.change_mode(mode)
+        else:
+            controller.change_mode("off")
+        controller.change_brightness(int(brightness) / 100)
+
+    modes = ['rainbow_run', 'rainbow_fade', 'solid_color']
+    return render_template('app.html', modes=modes, default_mode=mode)
 
 if __name__ == "__main__":
     app.run()
